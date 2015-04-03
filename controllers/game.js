34da5a4761eded38e8888e1382a90dedea50c5b3
Evan Weiler <plasmids@mykolab.com>
2015-04-03 12:40:13 -0500

    fixed for loops

--- controllers/game.js
+++ controllers/game.js
@@ -22,27 +22,37 @@ addPlayer = function(game, playerName) {
   game.joinQueue.push(new Player(playerName));
 }
 
+addQueuedPlayers = function(game) {
+  game.joinQueue.forEach( function(player){
+    game.players.push(player);
+  })
+  game.joinQueue = [];
+}
+
 initialize = function(game, foundingPlayerName) {
-  for (suit in ['Spades', 'Clubs', 'Hearts', 'Diamonds']) {
-      for (rank in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) {
+  ['Spades', 'Clubs', 'Hearts', 'Diamonds'].forEach(function(suit) {
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].forEach(function(rank) {
         game.deck.push(new Card(rank, suit));
-      }
-  }
+      });
+  });
   addPlayer(game, foundingPlayerName);
+  addQueuedPlayers(game);
   game.currentPlayer = foundingPlayerName;
   game.dealersCards.push(drawCard(game));
 }
 
 hasPlayer = function(game, playerName) {
-  for(player in game.players) {
+  inGame = false;
+  game.players.forEach( function(player) {
     if(playerName == player.playerName) {
-      return true;
+      inGame = true;
     }
-  }
-  return false;
+  })
+  return inGame;
 }
 
 exports.dealCard = dealCard;
 exports.initialize = initialize;
 exports.addPlayer = addPlayer;
 exports.hasPlayer = hasPlayer;
+exports.addQueuedPlayers = addQueuedPlayers;
