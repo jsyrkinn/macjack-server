1677ec2077b988f64a1f81f586441229d22ac4b2
Evan Weiler <plasmids@mykolab.com>
2015-04-03 12:41:46 -0500

    added test games and full state GET route

--- server.js
+++ server.js
@@ -2,21 +2,46 @@
 var server = require('http').createServer();
 var express = require('express');
 var Game = require('./models/game');
-var controller = require('.controllers/game');
+var controller = require('./controllers/game');
+require('mootools');
 
-app = express();
+var app = express();
 
 games = {};
 playerDict = {};
+
+// TEST
+
+playerDict[1] = 'Evan';
+playerDict[2] = 'Judy';
+
+testgame = new Game();
+controller.initialize(testgame, 'Evan');
+controller.addPlayer(testgame, 'Judy');
+controller.addQueuedPlayers(testgame);
+
+
+games[1] = testgame;
+
+//
 
 app.get('/games/:gameid/state.json', function(req, res) {
   gameID = req.params.gameid;
   if(gameID in games) {
     game = games[gameID];
-    if(req.playerID in playerDict) {
-      if(controller.hasPlayer(game, playerDict[req.playerID])) {
-        res.json(game);
+    if(req.query.playerid in playerDict) {
+      if(controller.hasPlayer(game, playerDict[req.query.playerid])) {
+        console.log(JSON.stringify(game.players[0]));
+        res.send(JSON.stringify(game));
+      } else {
+        res.status(404).send("Player not in game");
       }
+    } else {
+      res.status(404).send("Player not found");
     }
+  } else {
+    res.status(404).send("Game not found");
   }
 });
+
+app.listen(1337);
