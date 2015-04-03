2cb6a78c918f8850c1920c894937c8966368dd14
Evan Weiler <plasmids@mykolab.com>
2015-04-03 12:05:10 -0500

    made players start with one emtpy hand

--- models/player.js
+++ models/player.js
@@ -1,9 +1,10 @@
 
 var Hand = require('./hand');
-var Card = require('.card');
+var Card = require('./card');
 
 function Player(playerName) {
   this.hands = [];
+  this.hands.push(new Hand());
   this.active = true;
   this.totalMoney = 500;
   this.playerName = playerName;
