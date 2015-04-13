edc62be1ba38a337c8b29b1cb3024f4dbdf4ff90
Evan Weiler <plasmids@mykolab.com>
2015-04-12 19:51:48 -0500

    add playerName to player

--- models/player.js
+++ models/player.js
@@ -2,12 +2,13 @@
 var Hand = require('./hand');
 var Card = require('./card');
 
-function Player(playerID) {
+function Player(playerID, playerName) {
   this.hands = [];
   this.hands.push(new Hand());
   this.active = true;
   this.totalMoney = 500;
   this.playerID = playerID;
+  this.playerName = playerName;
 }
 
 module.exports = Player;
